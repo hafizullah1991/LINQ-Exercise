25b691453be2dab9591d6ace97c8f87f8abed23d
Hafiz Hamdard <ahmadshakhi2020@gmail.com>
2024-01-31 09:42:33 -0800

    Linq exercise it done

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> names = new List<string>() { "John", "Paul", "Hafiz", "Jacob", "Eve", "Cristina" };
+          var name1 =  names.OrderBy(x => x.Length).ThenBy(x => x);
+            var name2 = names.Where(x => x.Length > 3).ToList();
+            var name3 = names.Where(x => x.Length == 3).ToList();
+           foreach(string name in name3)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
